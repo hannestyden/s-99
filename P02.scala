022c729add78a4c6c6ba5ee455a4ebe28866d61d
Hannes Tydén <hannes@tyden.name>
2016-09-09 01:56:06 +0200

    P02 - Improvements

--- P02.scala
+++ P02.scala
@@ -4,10 +4,10 @@ object P02 {
   def example = penultimate(List(1, 1, 2, 3, 5, 8))
   def expectedValue = 5
 
-  private def penultimate[T](list: List[T]): T = list match {
-    case Nil                     => ???
-    case penultimate :: _ :: Nil => penultimate
-    case head :: tail            => penultimate(tail)
+  private def penultimate[A](list: List[A]): A = list match {
+    case head :: _ :: Nil => head
+    case _ :: tail        => penultimate(tail)
+    case Nil              => ???
   }
 
   def main(args: Array[String]) = {
