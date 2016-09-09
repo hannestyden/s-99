1395f7603c7510ded374498c3fe5cd97f79c228e
Hannes Tydén <hannes@tyden.name>
2016-09-09 23:34:49 +0200

    P04 - Merge solutions

--- P04.scala
+++ P04.scala
@@ -1,23 +1,37 @@
 import scala.annotation.tailrec
 
 object P04 {
-  val name = "P04"
+  val name = "P04 - Get length of list"
 
-  def example = length(List(1, 1, 2, 3, 5, 8))
-  def expectedValue = 6
+  def input  = List(1, 1, 2, 3, 5, 8)
+  def output = 6
+
+  def lengthRec[A](list: List[A]): Int = list match {
+    case Nil       => 0
+    case _ :: tail => 1 + lengthRec(tail)
+  }
 
   @tailrec
-  private def length[A](list: List[A], base: Int = 0): Int = list match {
+  def lengthTCO[A](list: List[A], base: Int = 0): Int = list match {
     case Nil       => base
-    case _ :: tail => length(tail, base + 1)
+    case _ :: tail => lengthTCO(tail, base + 1)
   }
 
   def main(args: Array[String]) = {
     println(name)
 
-    val actual = example
-    println(s"Actual:   $actual")
-    println(s"Expected: $expectedValue")
-    println(s"Equal:    ${actual == expectedValue}")
+    println(s"Input: $input")
+
+    println("Recursive")
+    compare(lengthRec(input), output)
+
+    println("Tail call optimized")
+    compare(lengthTCO(input), output)
+  }
+
+  private def compare[A](a: A, b: A) = {
+    println(s"Actual:   $a")
+    println(s"Expected: $b")
+    println(s"Equal:    ${a == b}")
   }
 }
