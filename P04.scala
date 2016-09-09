6c7ef529bb133207361c639e4eb82632a08beb35
Hannes Tydén <hannes@tyden.name>
2016-09-09 02:21:49 +0200

    P04 - Ensure tail call optimization

--- P04.scala
+++ P04.scala
@@ -1,9 +1,12 @@
+import scala.annotation.tailrec
+
 object P04 {
   val name = "P04"
 
   def example = length(List(1, 1, 2, 3, 5, 8))
   def expectedValue = 6
 
+  @tailrec
   private def length[A](list: List[A], base: Int = 0): Int = list match {
     case Nil       => base
     case _ :: tail => length(tail, base + 1)
