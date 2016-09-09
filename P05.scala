8714a85e6aafd41f24faee8a9f8d9e2d4e54daf0
Hannes Tydén <hannes@tyden.name>
2016-09-09 23:23:51 +0200

    P05 - Tail call optimization

--- P05.scala
+++ P05.scala
@@ -1,21 +1,39 @@
+import scala.annotation.tailrec
+
 object P05 {
-  val name = "P05"
+  val name = "P05 - Reverse list"
 
-  def example = reverse(List(1, 1, 2, 3, 5, 8))
-  def expectedValue = List(8, 5, 3, 2, 1, 1)
+  val input  = List(1, 1, 2, 3, 5, 8)
+  val output = List(8, 5, 3, 2, 1, 1)
 
-  private def reverse[A](list: List[A]): List[A] = list match {
+  def reverseRec[A](list: List[A]): List[A] = list match {
     case head :: Nil  => List(head)
-    case head :: tail => reverse(tail) ++ List(head)
+    case head :: tail => reverseRec(tail) ++ List(head)
+    case Nil          => ???
+  }
+
+  @tailrec
+  def reverseTCO[A](list: List[A], base: List[A] = List()): List[A] = list match {
+    case head :: Nil  => List(head) ++ base
+    case head :: tail => reverseTCO(tail, List(head) ++ base)
     case Nil          => ???
   }
 
   def main(args: Array[String]) = {
     println(name)
 
-    val actual = example
-    println(s"Actual:   $actual")
-    println(s"Expected: $expectedValue")
-    println(s"Equal:    ${actual == expectedValue}")
+    println(s"Input: $input")
+
+    println("Recursive")
+    compare(reverseRec(input), output)
+
+    println("Tail call optimized")
+    compare(reverseTCO(input), output)
+  }
+
+  private def compare[A](a: A, b: A) = {
+    println(s"Actual:   $a")
+    println(s"Expected: $b")
+    println(s"Equal:    ${a == b}")
   }
 }
