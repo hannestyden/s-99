eb9bfbe07c16ec5f8a20b2b611d58863a7a21916
Hannes Tydén <hannes@tyden.name>
2016-09-19 23:10:20 +0200

    P08 - Extensions

--- P08.scala
+++ P08.scala
@@ -3,17 +3,28 @@ import scala.annotation.tailrec
 object P08 {
   val name = "P08 - Eliminate consecutive duplicates"
 
-  val io: Seq[(List[Symbol], List[Symbol])] = Seq(
-    (List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e), List('a, 'b, 'c, 'a, 'd, 'e))
+  val inputsAndExpectedOutputs = Seq(
+    List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e) -> List('a, 'b, 'c, 'a, 'd, 'e),
+
+    List()               -> List(),
+    List('a)             -> List('a),
+    List('a, 'a, 'a, 'a) -> List('a),
+    List('a, 'b, 'a)     -> List('a, 'b, 'a)
   )
 
   // Naïve
-  def compress[A](list: List[A], prev: Option[A] = None): List[A] = list match {
-    case Nil => Nil
-    case head :: tail => {
-      if (Some(head) == prev) compress(tail, Some(head))
-      else                    head :: compress(tail, Some(head))
+  def compress[A](list: List[A]): List[A] = {
+    def _compress[A](list: List[A], prev: Option[A]): List[A] = {
+      list match {
+        case Nil => Nil
+        case head :: tail => {
+          if (Some(head) == prev) _compress(tail, Some(head))
+          else head :: _compress(tail, Some(head))
+        }
+      }
     }
+
+    _compress(list, None)
   }
 
   // @tailrec
@@ -25,15 +36,23 @@ object P08 {
   def main(args: Array[String]) = {
     println(name)
 
-    io.foreach { case (input: List[Symbol], output: List[Symbol]) => {
-      println(s"Input:    $input")
-      compare(compress(input), output)
-    } }
+    (Seq(
+      ("Recursive", compress _)
+    ): Seq[(String, List[Symbol] => List[Symbol])]).foreach { case (n, f) =>
+      println(n)
+      inputsAndExpectedOutputs.foreach { case (input: List[Symbol], output: List[Symbol]) => {
+        println("---")
+        report(input, f(input), output)
+      }}
+    }
+
+    println("---")
   }
 
-  private def compare[A](a: A, b: A) = {
-    println(s"Actual:   $a")
-    println(s"Expected: $b")
-    println(s"Equal:    ${a == b}")
+  private def report[A](i: A, a: A, e: A) = {
+    println(s"Input:           $i")
+    println(s"Actual output:   $a")
+    println(s"Expected output: $e")
+    println(s"Equal:           ${a == e}")
   }
 }
